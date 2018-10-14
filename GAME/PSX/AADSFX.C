d0edf4a88470e6b6bf1dd7d571454bd2b028c0b8
Gh0stBlade <Gh0stBlade@users.noreply.github.com>
2018-10-14 14:52:15 +0100

    Add stubs AADSFX.C

--- GAME/PSX/AADSFX.C
+++ GAME/PSX/AADSFX.C
@@ -14,7 +14,7 @@ unsigned long aadPlaySfx(unsigned short toneID, unsigned short volume, unsigned 
 	return handle;
 }
 
-unsigned long aadStopSfx(unsigned long handle /*$s0*/)
+unsigned long aadStopSfx(unsigned long handle)
 {
 	aadPutSfxCommand(1, 0, 0, handle, 0);
 	return handle;
@@ -119,71 +119,117 @@ void aadExecuteSfxCommand(struct AadSfxCommand* sfxCmd)
 	if (sfxCmd->statusByte < 9)
 	{
 		sfxCmdFunction[sfxCmd->statusByte]();//sfxCmdFunction = functionTbl, statusByte = Index
-
 	}
 	//loc_80056CB0
 	return;
 }
 
-unsigned short aadStopAllSlots()
+void sfxCmdPlayTone(struct AadSfxCommand* sfxCmd /*$s4*/)
 {
-	struct _AadSequenceSlot* slot;
-	unsigned short slotNumber;
-
-	slotNumber = 0;
-	if (aadMem->numSlots != 0)
-	{
-		//loc_80054638
-		for (slotNumber = 0; slotNumber < aadMem->numSlots; slotNumber++)
-		{
-			slot = aadMem->sequenceSlots[slotNumber];
-
-			if ((slot->status & 1))
-			{
-				aadStopSlot(slotNumber);
-			}
-			//loc_80054664
-			slot->sequenceNumberAssigned = 255;
-		}
-	}
-	//loc_80054684
-
-	return 0;
+	unsigned long handle; // $s7
+	struct AadProgramAtr* progAtr; // $fp
+	struct AadToneAtr* toneAtr; // $s2
+	struct AadSynthVoice* voice; // $s1
+	unsigned short midiNote; // $s5
+	unsigned long waveAddr; // $s6
+	struct AadLoadedSfxToneAttr* sfxToneAttr; // $s3
+	struct AadLoadedSfxWaveAttr* sfxWaveAttr; // $v0
+	int i; // $v1
 }
 
-unsigned short aadStopSlot(unsigned short slotNumber)
+void sfxCmdStopTone(struct AadSfxCommand* sfxCmd /*$a0*/)
 {
-	struct _AadSequenceSlot* slot;
-
-	if (slotNumber < aadMem->numSlots)
-	{
-		slot = aadMem->sequenceSlots[slotNumber];
-
-		if (slot->sequenceNumberAssigned != 0xFF)
-		{
-			slot->status &= 0xFFFE;
-			aadInitSequenceSlot(slot);
-			aadAllNotesOff(slotNumber);
-		}//loc_800545F4
-	}//loc_800545F4
-
-	return 0;
+	unsigned long handle; // $t0
+	struct AadSynthVoice* voice; // $a0
+	unsigned short i; // $a3
+	unsigned long vmask; // $v1
 }
 
-unsigned short aadShutdownReverb()
+void sfxCmdStopAllTones(struct AadSfxCommand* sfxCmd /*$a0*/)
 {
-	if (aadWaitForSramTransferComplete() != 0)
-	{
-		SpuClearReverbWorkArea(aadGetReverbMode());
-
-	}
-	
-	//loc_80054290
-	return 0;
+	struct AadSynthVoice* voice; // $a0
+	unsigned long vmask; // $v1
+	unsigned short i; // $a3
 }
 
-unsigned short aadCancelPauseSound()
+void sfxCmdSetToneVolumeAndPan(struct AadSfxCommand* sfxCmd /*$a0*/)
 {
-	aadMem->flags &= 0xFFF3;
-	return 0;
+	/*unsigned long handle; // $a3
+	struct AadSynthVoice *voice; // $t1
+	unsigned short i; // $t2
+	struct AadVolume newVoiceVol; // stack offset -16
+	{ // line 19, offset 0x8005703c
+		unsigned char tmp; // $v1
+	} // line 19, offset 0x8005703c
+	{ // line 20, offset 0x800570a4
+		unsigned long masterVolumeSquared; // $v1
+	} // line 20, offset 0x800570a4
+	{ // line 21, offset 0x80057124
+		unsigned char tmp; // $v1
+	} // line 21, offset 0x80057124
+	{ // line 22, offset 0x8005718c
+		unsigned long masterVolumeSquared; // $v1
+	} // line 22, offset 0x8005718c
+	{ // line 22, offset 0x8005718c
+		unsigned long masterVolumeSquared; // $v1
+	} // line 22, offset 0x8005718c
+	{ // line 22, offset 0x8005718c
+		unsigned long masterVolumeSquared; // $v0
+	} // line 22, offset 0x8005718c
+	{ // line 22, offset 0x8005718c
+		unsigned long masterVolumeSquared; // $v1
+	} // line 22, offset 0x8005718c*/
+}
+
+void sfxCmdSetToneVolPanPitch(struct AadSfxCommand* sfxCmd /*$s2*/)
+{
+	/*unsigned long handle; // $a2
+	struct AadSynthVoice *voice; // $s0
+	unsigned short i; // $s1
+	struct AadVolume newVoiceVol; // stack offset -24
+	unsigned short newPitch; // $a1
+	unsigned short finePitch; // $a1
+	unsigned short pitchIndex; // $a0
+	{ // line 20, offset 0x800573a8
+		unsigned char tmp; // $v1
+	} // line 20, offset 0x800573a8
+	{ // line 21, offset 0x80057410
+		unsigned long masterVolumeSquared; // $v1
+	} // line 21, offset 0x80057410
+	{ // line 22, offset 0x80057490
+		unsigned char tmp; // $v1
+	} // line 22, offset 0x80057490
+	{ // line 23, offset 0x800574f8
+		unsigned long masterVolumeSquared; // $v1
+	} // line 23, offset 0x800574f8
+	{ // line 23, offset 0x800574f8
+		unsigned long masterVolumeSquared; // $v1
+	} // line 23, offset 0x800574f8
+	{ // line 23, offset 0x800574f8
+		unsigned long masterVolumeSquared; // $v0
+	} // line 23, offset 0x800574f8
+	{ // line 23, offset 0x800574f8
+		unsigned long masterVolumeSquared; // $v1
+	} // line 23, offset 0x800574f8*/
+}
+
+void sfxCmdLockVoice(struct AadSfxCommand* sfxCmd /*$a0*/)
+{
+	void(*callbackProc)(); // $s0
+	struct AadSynthVoice *voice; // $v1
+}
+
+void sfxCmdSetVoiceAttr(struct AadSfxCommand* sfxCmd /*$a0*/)
+{
+	unsigned long v; // $s0
+	unsigned long vmask; // $v1
+	struct SpuVoiceAttr *voiceAttr; // $s1
+}
+
+void sfxCmdSetVoiceKeyOn(struct AadSfxCommand* sfxCmd /*$a0*/)
+{
+}
+
+void sfxCmdSetVoiceKeyOff(struct AadSfxCommand* sfxCmd /*$a0*/)
+{
 }
